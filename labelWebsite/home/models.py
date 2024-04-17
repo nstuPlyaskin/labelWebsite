008290cae0da133e9248ccbcc7ae129fdbf0639d
nstuPlyaskin <4deptmusic@gmail.com>
2024-04-18 05:40:45 +0700

    Added: homepage full design
    Added: homepage design css+html
    Added: news section on homepage
    Added: read news from db
    Added: every news have single object

--- labelWebsite/home/models.py
+++ labelWebsite/home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Articles(models.Model):
+    title = models.CharField('Название', max_length=42)
+    full_text = models.TextField('Статья', max_length=512)
+    date = models.DateTimeField('Дата публикации')
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
--- labelWebsite/home/views.py
+++ labelWebsite/home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Articles
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home/home.html')
+    news = Articles.objects.order_by('-date')
+    return render(request, 'home/home.html', {"news" : news})
